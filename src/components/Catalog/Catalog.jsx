7e466c57e79309a6a0e38f1cf63fa6161271efcb
AbShamilDev <abkarov02@gmail.com>
2024-05-26 03:53:18 +0300

    feat: fitler arrow

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,10 +1,27 @@
-import { createMusicParts } from '../SearchPage/musicData'
-import React, { useState, useEffect } from 'react';
-import './Catalog.scss';
+import { createMusicParts } from "../SearchPage/musicData";
+import React, { useState, useEffect } from "react";
+import "./Catalog.scss";
+import arrow from "../upload/arrow.svg";
+
+const FilterArrow = ({ sortConfig, objectNames, index }) => {
+  return (
+    sortConfig.key === objectNames[index] && (
+      <img
+        src={arrow}
+        alt="abracadabra"
+        className={sortConfig.direction === "ascending" && "rotate"}
+      />
+    )
+  );
+};
 
 const Catalog = () => {
-  const [musicParts, setMusicParts] = useState([]);
-  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+  const [musicParts, setMusicParts] = useState([{}]);
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: "ascending",
+  });
+  const objectNames = Object.keys(musicParts[0]);
 
   useEffect(() => {
     setMusicParts(createMusicParts());
@@ -15,10 +32,10 @@ const Catalog = () => {
     if (sortConfig.key) {
       sortableItems.sort((a, b) => {
         if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? -1 : 1;
+          return sortConfig.direction === "ascending" ? -1 : 1;
         }
         if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? 1 : -1;
+          return sortConfig.direction === "ascending" ? 1 : -1;
         }
         return 0;
       });
@@ -27,30 +44,79 @@ const Catalog = () => {
   }, [musicParts, sortConfig]);
 
   const requestSort = (key) => {
-    let direction = 'ascending';
-    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-      direction = 'descending';
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
     }
     setSortConfig({ key, direction });
   };
-
+  console.log(sortConfig.direction);
   return (
     <div className="MainBlock">
       <table className="musicTable">
         <thead>
           <tr>
-            <th onClick={() => requestSort('name')}><h3>Композитор</h3></th>
-            <th onClick={() => requestSort('song')}><h3>Название</h3></th>
-            <th onClick={() => requestSort('instrument')}><h3>Инструмент</h3></th>
-            <th onClick={() => requestSort('format')}><h3>Формат</h3></th>
-            <th onClick={() => requestSort('price')}><h3>Стоимость</h3></th>
+            <th onClick={() => requestSort(objectNames[0])}>
+              <div className="thContainer">
+                <h3>Композитор</h3>
+                <FilterArrow
+                  sortConfig={sortConfig}
+                  objectNames={objectNames}
+                  index={0}
+                />
+              </div>
+            </th>
+            <th onClick={() => requestSort(objectNames[1])}>
+              <div className="thContainer">
+                <h3>Название</h3>
+                <FilterArrow
+                  sortConfig={sortConfig}
+                  objectNames={objectNames}
+                  index={1}
+                />
+              </div>
+            </th>
+            <th onClick={() => requestSort(objectNames[2])}>
+              <div className="thContainer">
+                <h3>Инструмент</h3>
+                <FilterArrow
+                  sortConfig={sortConfig}
+                  objectNames={objectNames}
+                  index={2}
+                />
+              </div>
+            </th>
+            <th onClick={() => requestSort(objectNames[3])}>
+              <div className="thContainer">
+                <h3>Формат</h3>
+                <FilterArrow
+                  sortConfig={sortConfig}
+                  objectNames={objectNames}
+                  index={3}
+                />
+              </div>
+            </th>
+            <th onClick={() => requestSort(objectNames[4])}>
+              <div className="thContainer">
+                <h3>Стоимость</h3>
+                <FilterArrow
+                  sortConfig={sortConfig}
+                  objectNames={objectNames}
+                  index={4}
+                />
+              </div>
+            </th>
           </tr>
         </thead>
         <tbody>
           {sortedMusicParts.map((part, index) => (
             <tr key={index}>
-              <td><a href="/">{part.name}</a></td>
-              <td><a href="/">{part.song}</a></td>
+              <td>
+                <a href="/">{part.name}</a>
+              </td>
+              <td>
+                <a href="/">{part.song}</a>
+              </td>
               <td>{part.instrument}</td>
               <td>{part.format}</td>
               <td>{part.price}</td>
